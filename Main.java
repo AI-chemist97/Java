deaa3508f4fa9bd0d7b6251a987e5ed718d7bc32
hungry404 <bdjg1029@gmail.com>
2024-08-28 23:36:40 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner a,b,c = new Scanner(System.in);
+        // System.out.println("dd");
+        // System.out.println(Long.MAX_VALUE);
+        // System.out.println("dd");
+        // System.out.println(Long.MIN_VALUE);
+        
+        System.out.println(c);
+    }
+}
